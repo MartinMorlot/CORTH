28eb98f70a242d496a6df70e910a48daa5702459
MartinMorlot <martin.morlot@inrae.fr>
2025-09-22 15:20:56 +0200

    Made extraction script for both DB

--- Database/mdb_to_csv.sh
+++ Database/mdb_to_csv.sh
@@ -1,8 +1,23 @@
 #/usr/bin/sh
 
-FILENAME="Extracted_"
+FILENAME="Extracted_Loire_"
 csv_end=".csv"
 path_DB="/home/mmorlot/dev-work/CORTH/Database/DHMD.MDB"
+
+for table in $(mdb-tables -1 $path_DB); do
+    echo "Exporting $table..."
+
+    filename_csv="$FILENAME$table$csv_end"
+    echo "$filename_csv"
+    rm "$filename_csv"
+    mdb-export -D '%Y-%m-%d' "$path_DB" "$table" > "$filename_csv"
+done
+
+#/usr/bin/sh
+
+FILENAME="Extracted_Est_"
+csv_end=".csv"
+path_DB="/home/mmorlot/dev-work/CORTH/Database/BAREMEBASE-HYDRO_TU.MDB"
 
 for table in $(mdb-tables -1 $path_DB); do
     echo "Exporting $table..."
