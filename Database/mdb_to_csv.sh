214a7faea373cc89b97dd16e6dc69a9680788675
MartinMorlot <martin.morlot@inrae.fr>
2025-09-23 16:58:26 +0200

    Script to extract data from DB remade, data extracted

--- Database/mdb_to_csv.sh
+++ Database/mdb_to_csv.sh
@@ -1,29 +1,33 @@
 #/usr/bin/sh
 
-FILENAME="Extracted_Loire_"
 csv_end=".csv"
-path_DB="/home/mmorlot/dev-work/CORTH/Database/DHMD.MDB"
+path_DB="/home/mmorlot/dev-work/CORTH/Database/DB_files/"
 
-for table in $(mdb-tables -1 $path_DB); do
-    echo "Exporting $table..."
+declare -A DB_filename_dict
+DB_filename_dict["DHMD.MDB"]="Extracted_Loire_"
+DB_filename_dict["BAREMEBASE-HYDRO_TU.MDB"]="Extracted_Est_"
+DB_filename_dict["SARRE.MDB"]="Extracted_Sarre_"
+DB_filename_dict["SEMA67.MDB"]="Extracted_67_"
+DB_filename_dict["SEMA68.MDB"]="Extracted_68_"
 
-    filename_csv="$FILENAME$table$csv_end"
-    echo "$filename_csv"
-    rm "$filename_csv"
-    mdb-export -D '%Y-%m-%d' "$path_DB" "$table" > "$filename_csv"
+for db_file in "${!DB_filename_dict[@]}"; do
+
+    FILENAME=${DB_filename_dict[$db_file]}
+    path_DB_file="$path_DB$db_file"
+    echo "db_file: $db_file, filename: ${DB_filename_dict[$db_file]}"
+
+    
+
+    for table in $(mdb-tables -1 $path_DB_file); do
+        echo "Exporting $table..."
+
+        filename_csv="Extracted_file/$FILENAME$table$csv_end"
+        echo "$filename_csv"
+        rm "$filename_csv"
+        mdb-export -D '%Y-%m-%d' "$path_DB" "$table" > "$filename_csv"
+    done
 done
 
-#/usr/bin/sh
 
-FILENAME="Extracted_Est_"
-csv_end=".csv"
-path_DB="/home/mmorlot/dev-work/CORTH/Database/BAREMEBASE-HYDRO_TU.MDB"
 
-for table in $(mdb-tables -1 $path_DB); do
-    echo "Exporting $table..."
 
-    filename_csv="$FILENAME$table$csv_end"
-    echo "$filename_csv"
-    rm "$filename_csv"
-    mdb-export -D '%Y-%m-%d' "$path_DB" "$table" > "$filename_csv"
-done
