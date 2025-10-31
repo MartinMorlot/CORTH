40c617baba2c64e4902a33cac8bb979628738840
MartinMorlot <martin.morlot@inrae.fr>
2025-10-31 13:07:20 +0100

    New figures and latest changes

--- Regionalization_work/Regionalization_approach.R
+++ Regionalization_work/Regionalization_approach.R
@@ -69,22 +69,47 @@ k.max <- 2:10
 
 plots_best <- make_silhouette_and_wss_pot(cor_dist, hcut, 10, hc_func = "agnes", hc_method = "ward")
 
-hc <- hclust(as.hclust(cor_dist), method = "ward")
+png("Regionalization_work/Plots/number_cluster_fitting_wss.png", height = 1500, width = 2000, res = 300)
+plots_best[[1]]
+dev.off()
+
+png("Regionalization_work/Plots/number_cluster_fitting_silhouette.png", height = 1500, width = 2000, res = 300)
+plots_best[[2]]
+dev.off()
+
+hc <- as.hclust(agnes(cor_dist, method = "ward"))
 sub_grp_2 <- cutree(hc, k = 2)
 sub_grp_4 <- cutree(hc, k = 4)
 
+png("Regionalization_work/Plots/hclust_2group_agnes_ward.png", height = 2500, width = 2000, res = 300)
 plot(hc, cex = 0.6)
 rect.hclust(hc, k = 2, border = 2:3)
+dev.off()
 
+png("Regionalization_work/Plots/hclust_4group_agnes_ward.png", height = 2500, width = 2000, res = 300)
 plot(hc, cex = 0.6)
 rect.hclust(hc, k = 4, border = 2:5)
+dev.off()
 
-fviz_cluster(list(data = df, cluster = sub_grp_2))
+df_with_cluster <- df %>%
+    mutate(cluster = sub_grp_4)
+
+# TODO fix fviz_cluster to work with the data as it is
 fviz_cluster(list(data = df, cluster = sub_grp_4))
 
-# spaghetti par groupement 2 et 4
+fvoz
+
+
+# TODO make a start table by cluster and station and year
+cnames <- colnames()
+ggplot(df)
+
+# TODO make an end table by cluster and station and year
+# spaghetti par groupement de 4
 # start
-# end
+
+# TODO make an end table by cluster and station and year
 # date maxima
 
+# TODO make a geopackage with the clusters
 # carto
