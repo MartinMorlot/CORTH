06061d25a93807ea0cca8d13b00d3020e04ed8d7
MartinMorlot <martin.morlot@inrae.fr>
2025-11-10 16:47:35 +0100

    New DB and work

--- Regionalization_work/Regionalization_approach_per_variable.R
+++ Regionalization_work/Regionalization_approach_per_variable.R
@@ -12,13 +12,14 @@ library(gridExtra)
 
 source("Regionalization_work/Utility/Functions_for_regionalization.R")
 
-where_to_plot <- "Regionalization_work/Plots/Start/"
+year_to_analyze <- as.character("1986":"2025")
 
-
-full_cluster_analysis <- function(file_to_analyze, where_to_plot) {
+full_cluster_analysis <- function(file_to_analyze, where_to_plot, variable) {
     data_cluster <- read.csv(file_to_analyze)
     row.names(data_cluster) <- data_cluster$X
     df <- data_cluster[, -c(1, ncol(data_cluster))]
+
+    dir.create(where_to_plot, recursive = T)
 
     cor_dist <- as.dist(1 - cor(t(df), use = "pairwise.complete.obs"))
     cor_dist[is.na(cor_dist)] <- 2
@@ -30,32 +31,76 @@ full_cluster_analysis <- function(file_to_analyze, where_to_plot) {
 
     best_clustering_method <- distance_calc_clustering_method(cor_dist)
 
-    print(best_clustering_method)
+    sorted_clustering <- sort(best_clustering_method, decreasing = TRUE)
 
-    plots_best_agnes_ward <- make_silhouette_and_wss_pot(cor_dist, hcut, 10, hc_func = "agnes", hc_method = "ward")
+    print(sorted_clustering[1:3])
 
-    where_to_plot <- "Regionalization_work/Plots/Start/"
+    best_3 <- names(sorted_clustering)[1:3]
+
+    for (name in best_3) {
+        dir.create(paste0(where_to_plot, name), recursive = T)
+
+        plots <- make_silhouette_and_wss_pot(cor_dist, hcut, 10, hc_func = "agnes", hc_method = name)
+
+        wss_plot_location <- paste0(where_to_plot, name, "/number_cluster_fitting_wss.png")
+        print(wss_plot_location)
+        png(wss_plot_location, height = 1500, width = 2000, res = 300)
+        print(plots[[1]])
+        dev.off()
+
+        silhouette_plot_location <- paste0(where_to_plot, name, "/number_cluster_fitting_silhouette.png")
+        print(silhouette_plot_location)
+        png(silhouette_plot_location, height = 1500, width = 2000, res = 300)
+        print(plots[[2]])
+        dev.off()
+
+        for (number_of_clusters in 2:10) {
+            hc <- as.hclust(agnes(cor_dist, method = name))
+            sub_grp <- cutree(hc, k = number_of_clusters)
+
+            main_plot <- paste(variable, name, number_of_clusters)
+
+            nb_plot_loc <- paste0(where_to_plot, name, "/", number_of_clusters)
+
+            dir.create(nb_plot_loc, recursive = T)
+
+            cluster_number_plot_loc <- paste0(where_to_plot, name, "/", number_of_clusters, "/hclust.png")
+            png(cluster_number_plot_loc, height = 2500, width = 2000, res = 300)
+            plot(hc, cex = 0.6, main = name)
+            rect.hclust(hc, k = number_of_clusters, border = 2:3)
+            dev.off()
+
+            df_with_cluster <- df %>%
+                mutate(cluster = sub_grp)
+
+            cnames <- colnames(df_with_cluster)
+            cluster_col <- cnames[length(cnames)]
+            cluster_numbers <- unique(df_with_cluster[, cluster_col])
+
+            cluster_col_data <- df_with_cluster[, cluster_col]
 
 
-    wss_plot_location <- paste0(where_to_plot, "number_cluster_fitting_wss.png")
-    png(wss_plot_location, height = 1500, width = 2000, res = 300)
-    plots_best_agnes_ward[[1]]
-    dev.off()
+            for (cluster_number in seq_along(cluster_numbers)) {
+                row_sel <- which(cluster_col_data == cluster_number)
 
-    silhouette_plot_location <- paste0(where_to_plot, "number_cluster_fitting_silhouette.png")
-    png(silhouette_plot_location, height = 1500, width = 2000, res = 300)
-    plots_best_agnes_ward[[2]]
-    dev.off()
+                select_start_cluster <- df[row_sel, ]
+                plot_cluster(
+                    select_start_cluster, variable, cluster_number, year_to_analyze,
+                    nb_plot_loc
+                )
+            }
+        }
+    }
 }
 
 file_start <- "/home/mmorlot/dev-work/CORTH/Regionalization_work/Results/Per_cluster/start_data_cluster.csv"
-start_plot_loc <- "Regionalization_work/Plots/Start/"
+start_plot_loc <- "/home/mmorlot/dev-work/CORTH/Regionalization_work/Plots/Start/"
 
 file_end <- "/home/mmorlot/dev-work/CORTH/Regionalization_work/Results/Per_cluster/end_data_cluster.csv"
-end_plot_loc <- "Regionalization_work/Plots/End/"
+end_plot_loc <- "/home/mmorlot/dev-work/CORTH/Regionalization_work/Plots/End/"
 
 file_minima <- "/home/mmorlot/dev-work/CORTH/Regionalization_work/Results/Per_cluster/minima_data_cluster.csv"
-minima_plot_loc <- "Regionalization_work/Plots/Minima/"
+minima_plot_loc <- "/home/mmorlot/dev-work/CORTH/Regionalization_work/Plots/Minima/"
 
 
 
@@ -64,65 +109,6 @@ variables_to_iterate <- c("start", "end", "minima")
 for (variable in variables_to_iterate) {
     print(variable)
     file_loc <- get(paste0("file_", variable))
+    plot_loc <- get(paste0(variable, "_plot_loc"))
+    full_cluster_analysis(file_loc, plot_loc, variable)
 }
-
-
-
-
-
-start_data_cluster <- read.csv("/home/mmorlot/dev-work/CORTH/Regionalization_work/Results/Per_cluster/start_data_cluster.csv")
-row.names(start_data_cluster) <- start_data_cluster$X
-df_start <- start_data_cluster[, -c(1, ncol(start_data_cluster))]
-
-# make an end table by cluster and station and year
-end_data_cluster <- read.csv()
-row.names(end_data_cluster) <- end_data_cluster$X
-df_end <- end_data_cluster[, -c(1, ncol(end_data_cluster))]
-
-
-
-
-# make an minima table by cluster and station and year
-minima_data_cluster <- read.csv("/home/mmorlot/dev-work/CORTH/Regionalization_work/Results/Per_cluster/minima_data_cluster.csv")
-row.names(minima_data_cluster) <- minima_data_cluster$X
-df_minima <- minima_data_cluster[, -c(1, ncol(minima_data_cluster))]
-
-
-
-cor_dist_minima <- as.dist(1 - cor(t(df_minima), use = "pairwise.complete.obs"))
-cor_dist_minima[is.na(cor_dist_minima)] <- 2
-
-cor_dist_end <- as.dist(1 - cor(t(df_end), use = "pairwise.complete.obs"))
-cor_dist_end[is.na(cor_dist_end)] <- 2
-
-cor_dist_start <- as.dist(1 - cor(t(df_start), use = "pairwise.complete.obs"))
-cor_dist_start[is.na(cor_dist_start)] <- 2
-
-png("Regionalization_work/Plots/Start/Correlation_distance.png", height = 1500, width = 2000, res = 300)
-fviz_dist(cor_dist_start, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
-dev.off()
-
-png("Regionalization_work/Plots/End/Correlation_distance.png", height = 1500, width = 2000, res = 300)
-fviz_dist(cor_dist_end, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
-dev.off()
-
-png("Regionalization_work/Plots/Minima/Correlation_distance.png", height = 1500, width = 2000, res = 300)
-fviz_dist(cor_dist_minima, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
-dev.off()
-
-distance_calc_clustering_method(cor_dist_minima)
-
-
-distance_calc_clustering_method(cor_dist_end)
-
-
-distance_calc_clustering_method(cor_dist_start)
-
-
-k.max <- 2:10
-
-plots_best_minima <- make_silhouette_and_wss_pot(cor_dist_minima, hcut, 10, hc_func = "agnes", hc_method = "ward")
-
-plots_best_minima <- make_silhouette_and_wss_pot(cor_dist_minima, hcut, 10, hc_func = "agnes", hc_method = "ward")
-
-plots_best_minima <- make_silhouette_and_wss_pot(cor_dist_minima, hcut, 10, hc_func = "agnes", hc_method = "ward")
